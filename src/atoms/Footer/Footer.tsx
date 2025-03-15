bf7ccaba8b92e784a2d53d50c81de38d4bb50a33
Jakub Vrchlabsky <29353075+JakubVrch@users.noreply.github.com>
2025-03-15 18:15:34 +0000

    Add footer with disclaimer

---
+++ src/atoms/Footer/Footer.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Box, Text } from '@chakra-ui/react';
+
+const Footer: React.FC = () => {
+  return (
+    <Box as="footer" w="100%" py={1} px={8} bg="bg.muted" mt="auto">
+      <Text fontSize="2xs">
+        Disclaimer: This is a test tool for OIDC configurations. Not intended for production use. Use at your own risk.
+      </Text>
+    </Box>
+  );
+};
+
+export default Footer;
--- src/templates/Default/Default.tsx
+++ src/templates/Default/Default.tsx
@@ -2,6 +2,7 @@ import React from "react"
 import { Flex, Heading, VStack } from '@chakra-ui/react';
 
 import Navbar from "@/atoms/NavBar/NavBar";
+import Footer from "@/atoms/Footer/Footer";
 
 interface DefaultTemplateProps {
   title: string
@@ -9,14 +10,15 @@ interface DefaultTemplateProps {
 }
 
 const DefaultTemplate: React.FC<DefaultTemplateProps> = ({title, children}) => {
-
+  
   return (
-    <Flex direction="column" minHeight="100vh" mb="8">
+    <Flex direction="column" minHeight="100vh">
       <Navbar />
-      <VStack gap="8" mx="8" mt="1" direction="column" minHeight="100vh" alignItems="flex-start">
+      <VStack gap="8" mx="8" mt="1" direction="column" alignItems="flex-start" mb="8">
         <Heading as="h2">{title}</Heading>
         {children}
       </VStack>
+      <Footer />
     </Flex>
   );
 };
