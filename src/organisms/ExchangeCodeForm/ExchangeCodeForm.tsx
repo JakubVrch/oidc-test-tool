f93cb0b045dd265076b87173faccb0ca8f5f81a9
Jakub Vrchlabsky <29353075+JakubVrch@users.noreply.github.com>
2026-04-23 21:09:51 +0000

    Implement PKCE on ProcessReponse page

--- src/organisms/ExchangeCodeForm/ExchangeCodeForm.tsx
+++ src/organisms/ExchangeCodeForm/ExchangeCodeForm.tsx
@@ -1,34 +1,99 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { FormProvider, SubmitHandler, useForm } from "react-hook-form";
 import TextInput from "../../molecules/TextInput/TextInput";
+import CheckboxInput from "../../molecules/CheckboxInput/CheckboxInput";
 import Button from "@/atoms/Button/Button";
-import { HStack } from "@chakra-ui/react";
+import { Stack } from "@chakra-ui/react";
 
-interface ExchangeCodeFormData {
-  clientSecret: string;
+export interface ExchangeCodeFormData {
+  useClientSecret: boolean;
+  clientSecret?: string;
+  useCodeVerifier: boolean;
+  codeVerifier?: string;
 }
 
 interface ExchangeCodeFormProps {
   onSubmit: SubmitHandler<ExchangeCodeFormData>;
+  codeVerifier?: string | null;
 }
-const ExchangeCodeForm: React.FC<ExchangeCodeFormProps> = ({ onSubmit }) => {
-  const methods = useForm<ExchangeCodeFormData>();
+
+const ExchangeCodeForm: React.FC<ExchangeCodeFormProps> = ({
+  onSubmit,
+  codeVerifier,
+}) => {
+  const methods = useForm<ExchangeCodeFormData>({
+    defaultValues: {
+      useClientSecret: !codeVerifier,
+      clientSecret: "",
+      useCodeVerifier: !!codeVerifier,
+      codeVerifier: codeVerifier ?? "",
+    },
+  });
+
+  const { handleSubmit, watch, setError, clearErrors, trigger } = methods;
+  const useClientSecretChecked = watch("useClientSecret");
+  const useCodeVerifierChecked = watch("useCodeVerifier");
+
+  useEffect(() => {
+    if (codeVerifier != null) {
+      if (!useClientSecretChecked && !useCodeVerifierChecked) {
+        setError("useClientSecret", {
+          message: "Select at least one authentication method",
+        });
+      } else {
+        clearErrors("useClientSecret");
+      }
+    }
+    void trigger("clientSecret");
+  }, [useClientSecretChecked, useCodeVerifierChecked, codeVerifier, setError, clearErrors, trigger]);
+
+  useEffect(() => {
+    void trigger("codeVerifier");
+  }, [useCodeVerifierChecked, trigger]);
+
+  const onFormSubmit = (
+    data: ExchangeCodeFormData,
+    event?: React.BaseSyntheticEvent,
+  ) => {
+    if (codeVerifier != null && !data.useClientSecret && !data.useCodeVerifier) {
+      return;
+    }
+    onSubmit(data, event);
+  };
 
   return (
     <FormProvider {...methods}>
-      <form onSubmit={methods.handleSubmit(onSubmit)}>
-        <HStack gap="1em" alignItems="flex-end" maxW="30em">
+      <form onSubmit={handleSubmit(onFormSubmit)}>
+        <Stack gap="2" maxW="30em">
+          <CheckboxInput id="useClientSecret" label="Use Client Secret" />
           <TextInput
             id="clientSecret"
             label="Client Secret:"
             type="password"
-            registerOptions={{ required: "This field is required" }}
+            registerOptions={{
+              required: useClientSecretChecked ? "Client Secret is required" : false,
+            }}
           />
+          {codeVerifier != null && (
+            <>
+              <CheckboxInput id="useCodeVerifier" label="Use Code Verifier" />
+              <TextInput
+                id="codeVerifier"
+                label="Code Verifier:"
+                type="text"
+                defaultValue={codeVerifier}
+                registerOptions={{
+                  required: useCodeVerifierChecked ? "Code Verifier is required" : false,
+                }}
+              />
+            </>
+          )}
           <Button type="submit">Exchange Code</Button>
-        </HStack>
+        </Stack>
       </form>
     </FormProvider>
   );
 };
 
 export default ExchangeCodeForm;
+
