d3fd2e26bb9569c967da3445db4714442f66145e
Jakub Vrchlabsky <29353075+JakubVrch@users.noreply.github.com>
2026-05-31 20:02:29 +0000

    Simplify useConstructedUrl

--- src/organisms/ConstructRequestForm/useConstructedUrl.ts
+++ src/organisms/ConstructRequestForm/useConstructedUrl.ts
@@ -1,24 +1,11 @@
-import { useEffect, useState } from "react";
 import { UseFormWatch } from "react-hook-form";
 import { ConstructRequestFormValues } from "@/services/types/constructRequestForm";
 import { constructUrl } from "@/services/urlManager/urlManager";
 
 const useConstructedUrl = (watch: UseFormWatch<ConstructRequestFormValues>) => {
-  const [constructedUrl, setConstructedUrl] = useState<string | null>(null);
-
-  useEffect(() => {
-    const subscription = watch((values) => {
-      const result = constructUrl(values);
-      if (result.url) {
-        setConstructedUrl(result.url.toString());
-      } else {
-        setConstructedUrl(null);
-      }
-    });
-    return () => subscription.unsubscribe();
-  }, [watch]);
-
-  return constructedUrl;
+  const values = watch();
+  const result = constructUrl(values);
+  return result.url ?? null;
 };
 
 export default useConstructedUrl;
