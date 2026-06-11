bffc0a6ba76d1eb88c1f8647a01349353403b812
Jakub Vrchlabsky <29353075+JakubVrch@users.noreply.github.com>
2026-06-11 20:13:46 +0000

    Split built chunks

--- vite.config.ts
+++ vite.config.ts
@@ -10,4 +10,26 @@ export default defineConfig({
   server: {
     host: "0.0.0.0",
   },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (!id.includes("node_modules")) return;
+
+          if (id.includes("react-router-dom")) return "vendor-router";
+          if (
+            id.includes("@chakra-ui") ||
+            id.includes("@emotion") ||
+            id.includes("next-themes")
+          ) {
+            return "vendor-chakra";
+          }
+          if (id.includes("react-hook-form")) return "vendor-form";
+          if (id.includes("react-icons")) return "vendor-icons";
+          if (id.includes("react") || id.includes("scheduler"))
+            return "vendor-react";
+        },
+      },
+    },
+  },
 });
